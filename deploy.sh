252f179b0bb8bdfb815273b2316425993f5ea0fd
Pavel Ravits <vim55k@gmail.com>
2019-09-08 00:21:09 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,16 @@ STATUS="$(git status)"
 
 if [[ $STATUS == *"nothing to commit, working directory clean"* ]]
 then
+    cd www
+    yarn build
+    yarn export
+    touch out/.nojekyll
+    cd ..
+    
     sed -i '/out/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    # git push origin `git subtree split --prefix gh-pages master`:gh-pages --force
+    git push origin `git subtree split --prefix www/out dev`:master --force
     git reset HEAD~
     git checkout .gitignore
 else
