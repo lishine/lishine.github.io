7abfe0e72f378f25f7afac0bef889b61382338c3
Pavel Ravits <vim55k@gmail.com>
2019-09-13 14:16:55 +0300

    styled

--- www/src/features/index/projects/SBS.js
+++ www/src/features/index/projects/SBS.js
@@ -3,6 +3,13 @@ import { Box, Flex, H4, LI, UL, Grid, Span, NavLink, P } from 'styles/ss-compone
 import { ProjectTitle, SubRemark, Gallery } from './common'
 import { boxCss } from 'styles/ss-utils'
 import { mediaUp } from 'styles/utils'
+import styled from '@emotion/styled'
+
+const FeatureWrapper = styled(Box)()
+const FeatureSpan = styled(Span)()
+
+const DoesWrapper = styled(Box)()
+const DoesList = styled(UL)()
 
 export const SBS = props => (
     <Box {...props}>
@@ -26,31 +33,31 @@ export const SBS = props => (
                 // gridAutoFlow="column"
                 gridGap={2}
                 css={boxCss.css({
-                    [Span]: {
+                    [FeatureSpan]: {
                         textAlign: 'center',
                         border: '1px solid var(--onwhite-border)',
                         p: '5px',
                     },
-                    [Box]: {
+                    [FeatureWrapper]: {
                         textAlign: 'center',
                     },
                 })}
             >
-                <Box>
-                    <Span>Manage CRUD users</Span>
-                </Box>
-                <Box>
-                    <Span>Manage CRUD devices</Span>
-                </Box>
-                <Box>
-                    <Span>Live device alerts</Span>
-                </Box>
-                <Box>
-                    <Span>Control device</Span>
-                </Box>
-                <Box>
-                    <Span>Manage permissions</Span>
-                </Box>
+                <FeatureWrapper>
+                    <FeatureSpan>Manage CRUD users</FeatureSpan>
+                </FeatureWrapper>
+                <FeatureWrapper>
+                    <FeatureSpan>Manage CRUD devices</FeatureSpan>
+                </FeatureWrapper>
+                <FeatureWrapper>
+                    <FeatureSpan>Live device alerts</FeatureSpan>
+                </FeatureWrapper>
+                <FeatureWrapper>
+                    <FeatureSpan>Control device</FeatureSpan>
+                </FeatureWrapper>
+                <FeatureWrapper>
+                    <FeatureSpan>Manage permissions</FeatureSpan>
+                </FeatureWrapper>
             </Grid>
         </Flex>
 
@@ -62,47 +69,47 @@ export const SBS = props => (
                 [H4]: { textAlign: 'start' },
                 [LI]: { textAlign: 'start', mb: 1 },
 
-                div: {
+                [DoesWrapper]: {
                     mt: 2,
                     mie: 4,
                 },
-                [UL]: { mis: '1em' },
+                [DoesList]: { mis: '1em' },
                 [mediaUp('md')()]: {
                     maxHeight: '830px',
-                    div: {
+                    [DoesWrapper]: {
                         maxWidth: '50%',
                     },
                 },
                 [mediaUp('lg')()]: {
                     maxHeight: '530px',
-                    div: {
+                    [DoesWrapper]: {
                         maxWidth: '33%',
                     },
-                    [UL]: { mis: '-1em' },
+                    [DoesList]: { mis: '-1em' },
                 },
                 [mediaUp('xl')()]: {
                     maxHeight: '330px',
-                    div: {
+                    [DoesWrapper]: {
                         maxWidth: '25%',
                     },
                 },
             })}
         >
-            <Box>
+            <DoesWrapper>
                 <H4>API</H4>
-                <UL>
+                <DoesList>
                     <LI>Built as a services - serverless ready</LI>
                     <LI>SMS with Nexmo API</LI>
                     <LI>Auth with httpOnly cookie in JWT and SMS</LI>
                     <LI>Websockets server</LI>
                     <LI>Websockets test client in Codesandbox</LI>
                     <LI>Device Auth with Private/Public keys with libsodium</LI>
-                </UL>
-            </Box>
+                </DoesList>
+            </DoesWrapper>
 
-            <Box>
+            <DoesWrapper>
                 <H4>Deploy</H4>
-                <UL>
+                <DoesList>
                     <LI>Digital ocean</LI>
                     <LI>Everything in docker containers</LI>
                     <LI>Traefik reverse proxy + Letsencrypt</LI>
@@ -110,22 +117,22 @@ export const SBS = props => (
                     <LI>Single command Hasura console through ssh</LI>
                     <LI>Single command DB fill through ssh</LI>
                     <LI>Single command logs through ssh</LI>
-                </UL>
-            </Box>
+                </DoesList>
+            </DoesWrapper>
 
-            <Box>
+            <DoesWrapper>
                 <H4>DB</H4>
-                <UL>
+                <DoesList>
                     <LI>Hasura GraphQL engine</LI>
                     <LI>Migrations</LI>
                     <LI>Permissions</LI>
                     <LI>Postgress</LI>
-                </UL>
-            </Box>
+                </DoesList>
+            </DoesWrapper>
 
-            <Box>
+            <DoesWrapper>
                 <H4>Client</H4>
-                <UL>
+                <DoesList>
                     <LI>Next.js SSR dynamic deploy</LI>
                     <LI>SCSS, imported Bootstrap, Emotion, Styled-System</LI>
                     <LI>React state management: Easy-Peasy (Redux + React-hooks)</LI>
@@ -134,8 +141,8 @@ export const SBS = props => (
                         Latest CSS qualities: Flex, Grid, CSS custom and logical
                         properties
                     </LI>
-                </UL>
-            </Box>
+                </DoesList>
+            </DoesWrapper>
         </Flex>
 
         <Box mt={4} width="100%" maxWidth="600px">
--- www/src/features/index/projects/Salsa.js
+++ www/src/features/index/projects/Salsa.js
@@ -3,155 +3,165 @@ import { Box, Flex, H4, LI, UL, Grid, Span, NavLink } from 'styles/ss-components
 import { ProjectTitle, SubRemark, Gallery } from './common'
 import { boxCss } from 'styles/ss-utils'
 import { mediaUp } from 'styles/utils'
+import styled from '@emotion/styled'
 
-export const Salsa = props => (
-    <Box {...props}>
-        <ProjectTitle>Video courses of Salsa dancing</ProjectTitle>
-        <Flex mt={1} alignItems="flex-start" flexDirection={['column', null, 'row']}>
-            <Box mie={5}>
-                <SubRemark>Built from the ground up</SubRemark>
-                <Flex as="p" alignItems="center">
-                    <Span mie="1em">Have a look please:</Span>
-                    <NavLink href="https://do-salsa.tk" />
-                </Flex>
-            </Box>
-            <Grid
-                w={1}
-                maxWidth={['600px']}
-                flex={1}
-                mie={5}
-                mt={[3, null, '12px']}
-                justifyContent="space-between"
-                gridTemplateColumns="repeat(auto-fill, minmax(150px, 1fr))"
-                gridGap={2}
+const FeatureWrapper = styled(Box)()
+const FeatureSpan = styled(Span)()
+
+const DoesWrapper = styled(Box)()
+const DoesList = styled(UL)()
+
+export const Salsa = props => {
+    return (
+        <Box {...props}>
+            <ProjectTitle>Video courses of Salsa dancing</ProjectTitle>
+            <Flex mt={1} alignItems="flex-start" flexDirection={['column', null, 'row']}>
+                <Box mie={5}>
+                    <SubRemark>Built from the ground up</SubRemark>
+                    <Flex as="p" alignItems="center">
+                        <Span mie="1em">Have a look please:</Span>
+                        <NavLink href="https://do-salsa.tk" />
+                    </Flex>
+                </Box>
+                <Grid
+                    w={1}
+                    maxWidth={['600px']}
+                    flex={1}
+                    mie={5}
+                    mt={[3, null, '12px']}
+                    justifyContent="space-between"
+                    gridTemplateColumns="repeat(auto-fill, minmax(150px, 1fr))"
+                    gridGap={2}
+                    css={boxCss.css({
+                        [FeatureSpan]: {
+                            textAlign: 'center',
+                            border: '1px solid var(--onwhite-border)',
+                            p: '5px',
+                        },
+                        [FeatureWrapper]: {
+                            textAlign: 'center',
+                        },
+                    })}
+                >
+                    <FeatureWrapper>
+                        <FeatureSpan> Landing page </FeatureSpan>
+                    </FeatureWrapper>
+                    <FeatureWrapper>
+                        <FeatureSpan> Blog</FeatureSpan>
+                    </FeatureWrapper>
+                    <FeatureWrapper>
+                        <FeatureSpan> Buy course</FeatureSpan>
+                    </FeatureWrapper>
+                    <FeatureWrapper>
+                        <FeatureSpan> Learn course</FeatureSpan>
+                    </FeatureWrapper>
+                    <FeatureWrapper>
+                        <FeatureSpan> Manage content</FeatureSpan>
+                    </FeatureWrapper>
+                </Grid>
+            </Flex>
+
+            <Flex
+                textAlign="center"
+                flexDirection="column"
+                flexWrap="wrap"
                 css={boxCss.css({
-                    [Span]: {
-                        textAlign: 'center',
-                        border: '1px solid var(--onwhite-border)',
-                        p: '5px',
+                    [H4]: { textAlign: 'start' },
+                    [LI]: { textAlign: 'start', mb: 1 },
+
+                    [DoesWrapper]: {
+                        mt: 2,
+                        mie: 4,
                     },
-                    [Box]: {
-                        textAlign: 'center',
+                    [DoesList]: { mis: '1em' },
+                    [mediaUp('md')()]: {
+                        maxHeight: '830px',
+                        [DoesWrapper]: {
+                            maxWidth: '45%',
+                        },
+                    },
+                    [mediaUp('lg')()]: {
+                        maxHeight: '530px',
+                        [DoesWrapper]: {
+                            maxWidth: '33%',
+                        },
+                        [DoesList]: { mis: '-1em' },
+                    },
+                    [mediaUp('xl')()]: {
+                        maxHeight: '330px',
+                        [DoesWrapper]: {
+                            maxWidth: '25%',
+                        },
                     },
                 })}
             >
-                <Box>
-                    <Span> Landing page </Span>
-                </Box>
-                <Box>
-                    <Span> Blog</Span>
-                </Box>
-                <Box>
-                    <Span> Buy course</Span>
-                </Box>
-                <Box>
-                    <Span> Learn course</Span>
-                </Box>
-                <Box>
-                    <Span> Manage content</Span>
-                </Box>
-            </Grid>
-        </Flex>
+                <DoesWrapper>
+                    <H4>API</H4>
+                    <DoesList>
+                        <LI>
+                            Serverless services, deployed to Zeit Now v2 on top of AWS
+                            lambda
+                        </LI>
+                        <LI>JWT in cookie authentication</LI>
+                        <LI>Email service with MailGun API, MJML templates</LI>
+                    </DoesList>
+                </DoesWrapper>
 
-        <Flex
-            textAlign="center"
-            flexDirection="column"
-            flexWrap="wrap"
-            css={boxCss.css({
-                [H4]: { textAlign: 'start' },
-                [LI]: { textAlign: 'start', mb: 1 },
+                <DoesWrapper>
+                    <H4>Emails generator</H4>
+                    <DoesList>
+                        <LI>React rendering</LI>
+                        <LI>MJML markup</LI>
+                        <LI>DOT templates</LI>
+                    </DoesList>
+                </DoesWrapper>
 
-                div: {
-                    mt: 2,
-                    mie: 4,
-                },
-                [UL]: { mis: '1em' },
-                [mediaUp('md')()]: {
-                    maxHeight: '830px',
-                    div: {
-                        maxWidth: '45%',
-                    },
-                },
-                [mediaUp('lg')()]: {
-                    maxHeight: '530px',
-                    div: {
-                        maxWidth: '33%',
-                    },
-                    [UL]: { mis: '-1em' },
-                },
-                [mediaUp('xl')()]: {
-                    maxHeight: '330px',
-                    div: {
-                        maxWidth: '25%',
-                    },
-                },
-            })}
-        >
-            <Box>
-                <H4>API</H4>
-                <UL>
-                    <LI>
-                        Serverless services, deployed to Zeit Now v2 on top of AWS lambda
-                    </LI>
-                    <LI>JWT in cookie authentication</LI>
-                    <LI>Email service with MailGun API, MJML templates</LI>
-                </UL>
+                <DoesWrapper>
+                    <H4>Content managment</H4>
+                    <DoesList>
+                        <LI>DATO Headless CMS</LI>
+                        <LI>
+                            <NavLink href="https://www.datocms.com" display="inline" />
+                        </LI>
+                        <LI>GraphQL API</LI>
+                    </DoesList>
+                </DoesWrapper>
+
+                <DoesWrapper>
+                    <H4>DB</H4>
+                    <DoesList>
+                        <LI>Hasura GraphQL engine</LI>
+                        <LI>Docker on Heroku</LI>
+                        <LI>Migrations</LI>
+                        <LI>Permissions</LI>
+                        <LI>Postgress</LI>
+                    </DoesList>
+                </DoesWrapper>
+
+                <DoesWrapper>
+                    <H4>Client</H4>
+                    <DoesList>
+                        <LI>Next.js SSR dynamic deploy</LI>
+                        <LI>SEO optimized</LI>
+                        <LI>SCSS, imported Bootstrap, Emotion, Styled-System </LI>
+                        <LI>React state management: Easy-Peasy (Redux + React-hooks)</LI>
+                        <LI>PayPal API</LI>
+                        <LI>
+                            Design implemented according to design requirements given in
+                            Invision
+                        </LI>
+                        <LI>Responsive & adaptive design to 4 types of devices</LI>
+                        <LI>
+                            Latest CSS qualities: Flex, Grid, CSS custom and logical
+                            properties
+                        </LI>
+                    </DoesList>
+                </DoesWrapper>
+            </Flex>
+
+            <Box mt={4} width="100%" maxWidth="600px">
+                <Gallery images={['/img/salsa/1.png']} />
             </Box>
-
-            <Box>
-                <H4>Emails generator</H4>
-                <UL>
-                    <LI>React rendering</LI>
-                    <LI>MJML markup</LI>
-                    <LI>DOT templates</LI>
-                </UL>
-            </Box>
-
-            <Box>
-                <H4>Content managment</H4>
-                <UL>
-                    <LI>DATO Headless CMS</LI>
-                    <LI>
-                        <NavLink href="https://www.datocms.com" display="inline" />
-                    </LI>
-                    <LI>GraphQL API</LI>
-                </UL>
-            </Box>
-
-            <Box>
-                <H4>DB</H4>
-                <UL>
-                    <LI>Hasura GraphQL engine</LI>
-                    <LI>Docker on Heroku</LI>
-                    <LI>Migrations</LI>
-                    <LI>Permissions</LI>
-                    <LI>Postgress</LI>
-                </UL>
-            </Box>
-
-            <Box>
-                <H4>Client</H4>
-                <UL>
-                    <LI>Next.js SSR dynamic deploy</LI>
-                    <LI>SEO optimized</LI>
-                    <LI>SCSS, imported Bootstrap, Emotion, Styled-System </LI>
-                    <LI>React state management: Easy-Peasy (Redux + React-hooks)</LI>
-                    <LI>PayPal API</LI>
-                    <LI>
-                        Design implemented according to design requirements given in
-                        Invision
-                    </LI>
-                    <LI>Responsive & adaptive design to 4 types of devices</LI>
-                    <LI>
-                        Latest CSS qualities: Flex, Grid, CSS custom and logical
-                        properties
-                    </LI>
-                </UL>
-            </Box>
-        </Flex>
-
-        <Box mt={4} width="100%" maxWidth="600px">
-            <Gallery images={['/img/salsa/1.png']} />
         </Box>
-    </Box>
-)
+    )
+}
