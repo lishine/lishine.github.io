1ffdd503ba2e7d8a922e017eb6caa623e06a1c6f
Pavel Ravits <vim55k@gmail.com>
2019-09-17 21:14:37 +0300

    .

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,7 @@ then
     sed -i '/out/d' ./.gitignore
     git add .
     git commit -m "Edit .gitignore to publish"
-    git push origin `git subtree split --prefix www/out dev`:master --force
+    git push origin `git subtree split --prefix www/out dev`:master
     git reset HEAD~
     git checkout .gitignore
 else
