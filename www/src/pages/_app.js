a981349f1b05c6037d681e52e0f0e96a89708af7
Pavel Ravits <vim55k@gmail.com>
2019-09-21 19:02:43 +0300

    animation

--- www/src/pages/_app.js
+++ www/src/pages/_app.js
@@ -271,21 +271,23 @@ class MyApp extends App {
                                 ]}
                                 flex={1}
                             >
-                                <PageTransition
-                                    timeout={300}
-                                    classNames="page-transition"
-                                >
+                                {router.route === '/resume' ? (
                                     <Component {...pageProps} key={router.route} />
-                                </PageTransition>
+                                ) : (
+                                    <PageTransition
+                                        timeout={300}
+                                        classNames="page-transition"
+                                    >
+                                        <Component {...pageProps} key={router.route} />
+                                    </PageTransition>
+                                )}
                                 <style jsx global>{`
                                     .page-transition-enter {
                                         opacity: 0;
-                                        transform: translate3d(0, 20px, 0);
                                     }
                                     .page-transition-enter-active {
                                         opacity: 1;
-                                        transform: translate3d(0, 0, 0);
-                                        transition: opacity 300ms, transform 300ms;
+                                        transition: opacity 300ms;
                                     }
                                     .page-transition-exit {
                                         opacity: 1;
