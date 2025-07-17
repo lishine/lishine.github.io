b66c9a08e2d445ae8fc1461fb2cfa2001bf5004b
Pavel Ravits <vim55k@gmail.com>
2025-07-17 13:33:19 +0300

    Update Header.js

--- www/src/features/header/Header.js
+++ www/src/features/header/Header.js
@@ -7,6 +7,7 @@ import { Menu } from './Menu'
 export const Header = ({ mobileHeaderHeight }) => {
     return (
         <Box className="grid-container">
+            <div style="font-size:25px">4 YEARS NOT UPDATED</div>
             <Menu mobileHeaderHeight={mobileHeaderHeight} />
         </Box>
     )
